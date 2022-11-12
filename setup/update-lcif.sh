c1149cc9e155ae799b6068a8f5fb1137a84c5b03
Josh Ibad <jibad@ibad.one>
2022-11-12 10:23:09 -0800

    Fix bash scripts

--- setup/update-lcif.sh
+++ setup/update-lcif.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd lcif
 git stash && git stash clear
-git fetch origin && git pull origin master && npm install -y && npm run build && systemctl restart lcif
+git fetch origin && git pull origin main && npm install -y && npm run build && systemctl restart lcif
